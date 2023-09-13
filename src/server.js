2bfe9e908653daa7ee0d51b69b58c132f1458384
Luca Luz <94574250+lucaLuz@users.noreply.github.com>
2023-09-13 09:12:21 -0300

    okk

--- src/server.js
+++ src/server.js
@@ -9,13 +9,26 @@ app.use(express.json());
 
 app.post('/click', (req, res) => {
   const data = req.body;
-  fs.appendFileSync('registros.json', JSON.stringify(data) + '\n');
-  res.json(data);
+  try {
+    fs.appendFileSync('registros.json', JSON.stringify(data) + '\n');
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 app.get('/registros', (req, res) => {
-  const registros = fs.readFileSync('registros.json', 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
-  res.json(registros);
+  try {
+    const registros = fs.readFileSync('registros.json', 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
+    res.json(registros);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo deu errado!');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
